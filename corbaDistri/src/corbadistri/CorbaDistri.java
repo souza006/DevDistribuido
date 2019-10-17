629fcea755c2a71880f0e1d513bbb9481147bcee
Elvis Silva de Souza <elvis.souza@academico.ifrn.edu.br>
2019-10-17 10:29:41 -0300

    upando Corba

---
+++ corbaDistri/src/corbadistri/CorbaDistri.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package corbadistri;
+
+/**
+ *
+ * @author 20172014040027
+ */
+public class CorbaDistri {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    System.out.println("Teste123");
+    }
+    
+}
