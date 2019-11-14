eec2dc48e29468976de65b1119ea0c9067df1ab0
Elvis Silva de Souza <elvis.souza@academico.ifrn.edu.br>
2019-11-14 11:08:13 -0300

    soap completo

--- SOAP/Client/Client.py
+++ SOAP/Client/Client.py
@@ -1,47 +1,44 @@
+import zeep 
 from zeep import Client
 
-#Cria Classe Pokemon
 
-class Pokemon
+class Pokemon:
     pass
 
-#Define os atributos da classe e solicita os mesmos
 def novoPokemon():
     poke = Pokemon()
-    poke.nome = input('Digite o Nome do Pokémon: ')
-    poke.nick = input('Digite o Nick do Pokémon: ')
-    poke.numDex = int(input('Digite o número da Pokedex : '))
+    poke.nome = raw_input('Digite o Nome do Pokemon: ')
+    poke.nick = raw_input('Digite o Nick do Pokemon: ')
+    poke.numDex = int(raw_input('Digite o numero da Pokedex : '))
     return poke
 
-    def main():
-
-    	#
-
-
-        client = Client('http://localhost:8001/pokedex')
-        print('** Criador de Equipe Pokémon **')
+def main():
+	client = Client('http://localhost:8089/pokemon?wsdl')
+        print('** Criador de Equipe Pokemon **')
         while True:
-            print('\n Escolha uma das seguintes opções: ')
-            print('1) Inserir Pokémon')
-            print('2) Listar Equipe')
-            print('0) Encerrar Sessão')
-            op = input('> ')
-            if op == '1':
-                pokemon = novoPokemon()
+            print('\n Escolha uma das seguintes opcoes: ')
+            print('i) Inserir Pokemon')
+            print('l) Listar Equipe')
+            print('0) Encerrar Sessao')
+            op = raw_input('> ')
+            if op == 'i':
+                poke = novoPokemon()
                 client.service.insereParty(
                     poke.nome, poke.nick, poke.numDex
                 )
             
-            elif op == '2':
+            elif op == 'l':
                 listar = client.service.retornaParty()
                 for pokemon in listar:
                     print('#####')
-                    print('Numero Dex: ', pokemon.numDex)
-                    print('Nome do Pokemon: ', pokemon.nome)
-                    print('Nick do Pokemon: ', pokemon.nick)
+                    print('Numero Dex: ', poke.numDex)
+                    print('Nome do Pokemon: ', poke.nome)
+                    print('Nick do Pokemon: ', poke.nick)
             elif op == '0':
                 break
             else:
-                print('Operação não definida')
+                print('Opcao nao definida')
 
-            
+          
+if __name__ == '__main__':
+    main()
