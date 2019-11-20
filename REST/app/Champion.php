d7fda6ff45f92f5a3c1b295b10275df64a7a3845
Elvis Souza <souzaelvis33@gmail.com>
2019-11-19 23:44:20 -0300

    upando rest iniciado

---
+++ REST/app/Champion.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Champion extends Model
+{
+    protected $fillable = [
+        'nome', 'tipo', 'terraNatal',
+    ];
+
+}
